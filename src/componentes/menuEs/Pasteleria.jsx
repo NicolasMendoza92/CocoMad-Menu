8714a625f01ba661e341cc39276fe7550faca2e8
NicoMen <nicomendoza.92@gmail.com>
2022-03-21 12:00:52 +0100

    actualizacion precios menu

--- src/componentes/menuEs/Pasteleria.jsx
+++ src/componentes/menuEs/Pasteleria.jsx
@@ -7,7 +7,7 @@ export const Pasteleria = () => {
             <Accordion.Item className='menu-button1' eventKey="0">
                 <Accordion.Header>PASTELERIA</Accordion.Header>
                 <Accordion.Body className='d-flex align-items-center justify-content-center m-2'>
-                   <img src="https://res.cloudinary.com/dcx1rcwvu/image/upload/v1647780007/cocoMAD/Menu/pasteleria_Es_ezoaux.png" alt=""/>
+                   <img src="https://res.cloudinary.com/dcx1rcwvu/image/upload/v1647860329/cocoMAD/Menu/pasteleria_Es_gcpwi6.png" alt=""/>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
